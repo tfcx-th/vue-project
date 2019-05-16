caa261ce81fd4ac0f85f7a6f0f779df8ef2f42db
tfcx-th <1438994285@qq.com>
2019-05-16 21:13:41 +0800

    update 1px border

--- eleme-imitation/src/App.vue
+++ eleme-imitation/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <v-header></v-header>
-    <div class="tab">
+    <div class="tab border-1px">
       <div class="tab-item">
         <router-link to="/goods">商品</router-link>
       </div>
@@ -27,13 +27,15 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+@import "./common/stylus/mixin.styl";
+
 #app
   .tab
     display flex
     width 100%
     height 40px
     line-height 40px
-    border-bottom 1px solid rgba(7, 17, 27, 0.1)
+    border-1px(rgba(7, 17, 27, 0.1))
     .tab-item
       flex: 1
       text-align: center
