3fa003e16bf0359317d710224c5f40ad78be6356
tfcx-th <1438994285@qq.com>
2019-05-17 21:30:52 +0800

    Update header.vue

--- eleme-imitation/src/components/header/header.vue
+++ eleme-imitation/src/components/header/header.vue
@@ -13,12 +13,23 @@
           {{ seller.description }}/{{ seller.deliveryTime}}分钟送达
         </div>
         <div v-if="seller.supports" class="support">
-          <span class="icon"></span>
+          <span class="icon" :class="classMap[seller.supports[0].type]"></span>
           <span class="text">{{ seller.supports[0].description }}</span>
         </div>
       </div>
+      <div v-if="seller.supports" class="support-count" @click="showDetail">
+        <span class="count">{{ seller.supports.length }}个</span>
+        <i class="icon-keyboard_arrow_right"></i>
+      </div>
     </div>
-    <div class="bulletin-wrapper"></div>
+    <div class="bulletin-wrapper" @click="showDetail">
+      <span class="bulletin-title"></span><span class="bulletin-text">{{ seller.bulletin }}</span>
+      <i class="icon-keyboard_arrow_right"></i>
+    </div>
+    <div class="background">
+      <img :src="seller.avatar" width="100%" height="100%">
+    </div>
+    <div v-show="detailShow" class="detail"></div>
   </div>
 </template>
 <script>
@@ -27,6 +38,19 @@ export default {
     seller: {
       type: Object
     }
+  },
+  data () {
+    return {
+      detailShow: false
+    }
+  },
+  methods: {
+    showDetail () {
+      this.detailShow = true;
+    }
+  },
+  created () {
+    this.classMap = ['decrease', 'discount', 'guarantee', 'invoice', 'special'];
   }
 }
 </script>
@@ -34,9 +58,12 @@ export default {
 @import "../../common/stylus/mixin";
 
 .header
+  position relative
+  overflow hidden
   color #fff
-  background #002
+  background rgba(7, 17, 27, 0.5)
   .content-wrapper
+    position relative
     padding 24px 12px 18px 24px
     font-size 0
     .avatar
@@ -69,6 +96,7 @@ export default {
       .support
         .icon
           display inline-block
+          vertical-align top
           width 12px
           height 12px
           margin-right 4px
@@ -84,4 +112,68 @@ export default {
             bg-image('invoice_1')
           &.special
             bg-image('special_1')
+        .text
+          line-height 12px
+          font-size 10px
+    .support-count
+      position absolute
+      right 12px
+      bottom 14px
+      padding 0 8px
+      height 24px
+      line-height 24px
+      border-radius 14px
+      background rgba(0, 0, 0, 0.2)
+      text-align center
+      .count
+        vertical-align top
+        font-size 10px
+      .icon-keyboard_arrow_right
+        margin-left 2px
+        line-height 24px
+        font-size 10px
+  .bulletin-wrapper
+    position relative
+    height 28px
+    line-height 28px
+    padding 0 22px 0 12px
+    white-space nowrap
+    overflow hidden
+    text-overflow ellipsis
+    background rgba(7, 17, 27, 0.2)
+    .bulletin-title
+      display inline-block
+      vertical-align top
+      margin-top 8px
+      width 22px
+      height 12px
+      bg-image('bulletin')
+      background-size 22px 12px
+      background-repeat no-repeat
+    .bulletin-text
+      vertical-align top
+      margin 0 4px
+      font-size 10px
+    .icon-keyboard_arrow_right
+      position absolute
+      font-size 10px
+      right 12px
+      top 8px
+  .background
+    position absolute
+    top 0
+    left 0
+    width 100%
+    height 100%
+    z-index -1
+    filter blur(10px)
+  .detail
+    position fixed
+    z-index 100
+    top 0
+    left 0
+    width 100%
+    height 100%
+    overflow auto
+    background rgba(7, 17, 27, 0.8)
 </style>
