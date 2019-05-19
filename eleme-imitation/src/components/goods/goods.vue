586c04d9e522c7232c5befd15e74b647e3161098
tfcx-th <1438994285@qq.com>
2019-05-19 21:54:39 +0800

    Update goods.vue
    实现左右双栏联动

--- eleme-imitation/src/components/goods/goods.vue
+++ eleme-imitation/src/components/goods/goods.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="goods">
-    <div class="menu-wrapper">
+    <div class="menu-wrapper" ref="menuWrapper">
       <ul>
-        <li v-for="(item, index) in goods" :key="index" class="menu-item">
+        <li v-for="(item, index) in goods"
+             :key="index"
+            class="menu-item"
+           :class="{'current':currentIndex===index}"
+            @click="selectMenu(index, $event)"
+              ref="menuList">
           <span class="text border-1px">
             <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>
             {{ item.name }}
@@ -10,9 +15,9 @@
         </li>
       </ul>
     </div>
-    <div class="foods-wrapper">
+    <div class="foods-wrapper" ref="foodsWrapper">
       <ul>
-        <li v-for="(item, index) in goods" :key="index" class="food-list">
+        <li v-for="(item, index) in goods" :key="index" class="food-list" ref="foodList">
           <h1 class="title">{{ item.name }}</h1>
           <ul>
             <li v-for="(food, index) in item.foods" :key="index" class="food-item border-1px">
@@ -51,7 +56,21 @@ export default {
   },
   data () {
     return {
-      goods: []
+      goods: [],
+      listHeight: [],
+      scrollY: []
+    }
+  },
+  computed: {
+    currentIndex () {
+      for (let i = 0; i < this.listHeight.length; i++) {
+        let height = this.listHeight[i];
+        let height2 = this.listHeight[i + 1];
+        if (!height2 || (this.scrollY >= height && this.scrollY < height2)) {
+          return i;
+        }
+      }
+      return 0;
     }
   },
   created () {
@@ -59,25 +78,49 @@ export default {
       response = response.body;
       if (response.errno === ERR_OK) {
         this.goods = response.data;
-        // this.$nextTick(() => {
-        //   this.scroll = new Bscroll(this.$refs.foods-wrapper, {})
-        // });
+        this.$nextTick(() => {
+          this._initScroll();
+          this._calculateHeight();
+          // this.selectMenu()
+        })
       }
     });
     this.classMap = ['decrease', 'discount', 'guarantee', 'invoice', 'special'];
   },
-  mounted () {
-    this.$nextTick(() => {
-      this.menuScroll = new BScroll('.menu-wrapper', {});
-      this.foodScroll = new BScroll('.foods-wrapper', {});
-    })
+  methods: {
+    selectMenu (index, event) {
+      if (!event._constructed) {
+        return;
+      }
+      let foodList = this.$refs.foodList;
+      let el = foodList[index];
+      this.foodsScroll.scrollToElement(el, 500);
+    },
+    _initScroll () {
+      this.meunScroll = new BScroll(this.$refs.menuWrapper, {
+        click: true
+      });
+      this.foodsScroll = new BScroll(this.$refs.foodsWrapper, {
+        click: true,
+        probeType: 3
+      });
+      this.foodsScroll.on('scroll', (pos) => {
+        if (pos.y <= 0) {
+          this.scrollY = Math.abs(Math.round(pos.y));
+        }
+      });
+    },
+    _calculateHeight () {
+      let foodList = this.$refs.foodList;
+      let height = 0;
+      this.listHeight.push(height);
+      for (let i = 0; i < foodList.length; i++) {
+        let item = foodList[i];
+        height += item.clientHeight;
+        this.listHeight.push(height);
+      }
+    }
   }
-  // methods: {
-  //   _initScroll () {
-  //     this.menuScroll = new BScroll(document.querySelector('.menu-wrapper'));
-  //     this.foodsScroll = new BScroll(document.querySelector('foods-wrapper'));
-  //   }
-  // }
 }
 </script>
 <style lang="stylus" rel="stylesheet/stylus">
@@ -100,6 +143,14 @@ export default {
       width 56px
       padding 0 12px
       line-height 14px
+      &.current
+        position relative
+        z-index 10
+        margin-top -1px
+        background #ffffff
+        font-weight 700
+        .text
+          border-none()
       .icon
         display inline-block
         vertical-align top
@@ -158,9 +209,10 @@ export default {
           font-size 10px
           color rgb(147, 153, 159)
         .desc
+          line-height 12px
           margin-bottom 8px
         .extra
-          &.count
+          .count
             margin-right 12px
         .price
           font-weight 700
