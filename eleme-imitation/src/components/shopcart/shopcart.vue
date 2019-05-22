89f50583d8ed65df873f9b492658eb979bafa8eb
tfcx-th <1438994285@qq.com>
2019-05-22 19:57:21 +0800

    update shopcart

--- eleme-imitation/src/components/shopcart/shopcart.vue
+++ eleme-imitation/src/components/shopcart/shopcart.vue
@@ -3,12 +3,13 @@
     <div class="content">
       <div class="content-left">
         <div class="logo-wrapper">
-          <div class="logo">
-            <span class="icon-shopping_cart"></span>
+          <div class="logo" :class="{'highlight':totalCount>0}">
+            <span class="icon-shopping_cart" :class="{'highlight':totalCount>0}"></span>
           </div>
+          <div class="num" v-show="totalCount>0">{{ totalCount }}</div>
         </div>
-        <div class="price">￥0</div>
-        <div class="desc">另需配送费{{ deliveryPrice }}元</div>
+        <div class="price" :class="{'highlight':totalPrice>0}">￥{{ totalPrice }}</div>
+        <div class="desc">另需配送费￥{{ deliveryPrice }}元</div>
       </div>
       <div class="content-right">
         <div class="pay">
@@ -21,6 +22,12 @@
 <script type="text/ecmascript-6">
 export default {
   props: {
+    selectFoods: {
+      type: Array,
+      default () {
+        return [];
+      }
+    },
     deliveryPrice: {
       type: Number,
       default: 0
@@ -28,6 +35,22 @@ export default {
     minPrice: {
       type: Number,
       default: 0
+    }
+  },
+  computed: {
+    totalPrice () {
+      let total = 0;
+      this.selectFoods.forEach((food) => {
+        total += food.price * food.count;
+      });
+      return total;
+    },
+    totalCount () {
+      let count = 0;
+      this.selectFoods.forEach((food) => {
+        count += food.count;
+      })
+      return count;
     }
   }
 }
@@ -66,10 +89,28 @@ export default {
           border-radius 50%
           text-align center
           background #2b343c
+          &.highlight
+            background rgb(0, 160, 220)
           .icon-shopping_cart
             line-height 44px
             font-size 24px
             color #80858a
+            &.highlight
+              color #ffffff
+        .num
+          position absolute
+          top 0
+          right 0
+          width 24px
+          height 16px
+          line-height 16px
+          text-align center
+          border-radius 16px
+          font-size 9px
+          font-weight 700
+          color #ffffff
+          background rgb(240, 20, 20)
+          box-shadow 0 4px 8px 0 rgba(0, 0, 0, 0.4)
       .price
         display inline-block
         vertical-align top
@@ -80,6 +121,8 @@ export default {
         border-right 1px solid rgba(255, 255, 255, 0.1)
         font-size 16px
         font-weight 700
+        &.highlight
+          color #ffffff
       .desc
         display inline-block
         vertical-align top
