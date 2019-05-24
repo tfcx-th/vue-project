369fdb7e3264bdb8945fa38a15672f0805538628
tfcx-th <1438994285@qq.com>
2019-05-24 21:47:34 +0800

    Update shopcart.vue
    添加购物车折叠窗口

--- eleme-imitation/src/components/shopcart/shopcart.vue
+++ eleme-imitation/src/components/shopcart/shopcart.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="shopcart">
-    <div class="content">
+    <div class="content" @click="toggleList">
       <div class="content-left">
         <div class="logo-wrapper">
           <div class="logo" :class="{'highlight':totalCount>0}">
@@ -26,10 +26,36 @@
         </transition>
       </div>
     </div>
+    <transition name="fold">
+      <div class="shopcart-list" v-show="listShow">
+        <div class="background"></div>
+        <div class="list-header">
+          <h1 class="title">购物车</h1>
+          <span class="empty" @click="empty">清空</span>
+        </div>
+        <div class="list-content" ref="listContent">
+          <ul>
+            <li class="food" v-for="(food, index) in selectFoods" :key="index">
+              <span class="name">{{ food.name }}</span>
+              <div class="price">
+                <span>￥{{ food.price * food.count }}</span>
+              </div>
+              <div class="cartcontrol-wrapper">
+                <cartcontrol :food="food"></cartcontrol>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </transition>
+    <transition name="fade">
+      <div class="shopcart-list-background" v-show="listShow"></div>
+    </transition>
   </div>
 </template>
 <script type="text/ecmascript-6">
 import cartcontrol from '../cartcontrol/cartcontrol'
+import BScroll from 'better-scroll';
 
 export default {
   props: {
@@ -70,7 +96,8 @@ export default {
           show: false
         }
       ],
-      dropBalls: []
+      dropBalls: [],
+      fold: true
     }
   },
   computed: {
@@ -102,6 +129,25 @@ export default {
       if (this.totalPrice >= this.minPrice) {
         return 'enough';
       }
+    },
+    listShow () {
+      if (!this.totalCount) {
+        this.fold = true;
+        return false;
+      }
+      let show = !this.fold;
+      if (show) {
+        this.$nextTick(() => {
+          if (!this.scroll) {
+            this.scroll = new BScroll(this.$refs.listContent, {
+              click: true
+            });
+          } else {
+            this.scroll.refresh();
+          }
+        });
+      }
+      return show;
     }
   },
   methods: {
@@ -153,11 +199,24 @@ export default {
         ball.show = false;
         el.style.display = 'none';
       }
+    },
+    toggleList () {
+      if (!this.totalCount) {
+        return;
+      }
+      this.fold = !this.fold;
+    },
+    empty () {
+      this.selectFoods.forEach((food) => {
+        food.count = 0;
+      })
     }
   }
 }
 </script>
 <style lang="stylus" rel="stylesheet/stylus">
+@import "../../common/stylus/mixin";
+
 .shopcart
   position fixed
   left 0
@@ -257,4 +316,70 @@ export default {
         border-radius 50%
         background rgb(0, 160, 220)
         transition all 0.4s linear
+  .shopcart-list
+    position absolute
+    left 0
+    top 0
+    z-index -1
+    width 100%
+    transform translate3d(0, -100%, 0)
+    &.fold-enter-active, &.fold-leave-active
+      transition all 0.5s
+    &.fold-enter, &.fold-leave-active
+      transform translate3d(0, 0, 0)
+    .list-header
+      height 40px
+      line-height 40px
+      padding 0 18px
+      background #f3f5f7
+      border-bottom 1px solid rgba(7, 17, 27, 0.1)
+      .title
+        float left
+        font-size 14px
+        color rgb(7, 17, 27)
+      .empty
+        float right
+        font-size 12px
+        color rgb(0, 160, 220)
+    .list-content
+      padding 0 18px
+      max-height 217px
+      overflow hidden
+      background #fff
+      .food
+        position relative
+        padding 12px 0
+        box-sizing border-box
+        border-1px(rgba(7, 17, 27, 0.1))
+        .name
+          line-height 24px
+          font-size 14px
+          color rgb(7, 17, 27)
+        .price
+          position absolute
+          right 90px
+          bottom 12px
+          line-height 24px
+          font-size 14px
+          font-weight 700
+          color rgb(240, 20, 20)
+        .cartcontrol-wrapper
+          position absolute
+          right 0
+          bottom 6px
+  .shopcart-list-background
+    position fixed
+    top 0
+    left 0
+    width 100%
+    height: 100%
+    z-index -10
+    backdrop-filter blur(10px)
+    opacity 1
+    background rgba(7, 17, 27, 0.6)
+    &.fade-enter-active, &.fade-leave-active
+      transition: all 0.5s
+    &.fade-enter, &.fade-leave-active
+      opacity: 0
+      background: rgba(7, 17, 27, 0)
 </style>
