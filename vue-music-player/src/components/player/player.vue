a67587c6e2dcb8db253a7870d8c5ac2e1da7224d
tfcx-th <tonghao_xjtu@qq.com>
2019-07-10 21:14:44 +0800

    feat:drag progress-bar

--- vue-music-player/src/components/player/player.vue
+++ vue-music-player/src/components/player/player.vue
@@ -26,18 +26,25 @@
           </div>
         </div>
         <div class="bottom">
+          <div class="progress-wrapper">
+            <span class="time time-l">{{ format(currentTime) }}</span>
+            <div class="progress-bar-wrapper">
+              <progress-bar :percent="percent" @percentChange="onProgressBarChange"></progress-bar>
+            </div>
+            <span class="time time-r">{{ format(currentSong.duration) }}</span>
+          </div>
           <div class="operators">
             <div class="icon i-left">
               <i class="icon-sequence"></i>
             </div>
-            <div class="icon i-left">
-              <i class="icon-prev"></i>
+            <div class="icon i-left" :class="disableCLass">
+              <i class="icon-prev" @click="prev"></i>
             </div>
-            <div class="icon i-center">
+            <div class="icon i-center" :class="disableCLass">
               <i :class="playIcon" @click="togglePlaying"></i>
             </div>
-            <div class="icon i-right">
-              <i class="icon-next"></i>
+            <div class="icon i-right" :class="disableCLass">
+              <i class="icon-next" @click="next"></i>
             </div>
             <div class="icon i-right">
               <i class="icon-not-favorite"></i>
@@ -63,18 +70,31 @@
         </div>
       </div>
     </transition>
-    <audio :src="currentSong.url" ref="audio"></audio>
+    <audio :src="currentSong.url" ref="audio"
+           @canplay="ready"
+           @error="error"
+           @timeupdate="updateTime"></audio>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
 import {mapGetters, mapMutations} from 'vuex';
 import animations from 'create-keyframe-animation';
-import {prefixStyle} from 'common/js/dom'
+import {prefixStyle} from 'common/js/dom';
+import ProgressBar from 'base/progress-bar/progress-bar';
 
 const transform = prefixStyle('transform');
 
 export default {
+  components: {
+    ProgressBar
+  },
+  data () {
+    return {
+      songReady: false,
+      currentTime: 0
+    }
+  },
   computed: {
     cdClass () {
       return this.playing ? 'play' : 'play pause'
@@ -85,11 +105,18 @@ export default {
     miniIcon () {
       return this.playing ? 'icon-pause-mini' : 'icon-play-mini';
     },
+    disableCLass () {
+      return this.songReady ? '' : 'disable';
+    },
+    percent () {
+      return this.currentTime / this.currentSong.duration;
+    },
     ...mapGetters([
       'fullScreen',
       'playlist',
       'currentSong',
-      'playing'
+      'playing',
+      'currentIndex'
     ])
   },
   methods: {
@@ -139,6 +166,52 @@ export default {
     togglePlaying () {
       this.setPlayingState(!this.playing);
     },
+    prev () {
+      if (!this.songReady) return;
+      let index = this.currentIndex - 1;
+      if (index === -1) {
+        index = this.playlist.length - 1;
+      }
+      this.setCurrentIndex(index);
+      if (!this.playing) {
+        this.togglePlaying();
+      }
+      this.songReady = false;
+    },
+    next () {
+      if (!this.songReady) return;
+      let index = this.currentIndex + 1;
+      if (index === this.playlist.length) {
+        index = 0;
+      }
+      this.setCurrentIndex(index);
+      if (!this.playing) {
+        this.togglePlaying();
+      }
+      this.songReady = false;
+    },
+    ready () {
+      this.songReady = true;
+    },
+    error () {
+      this.songReady = true;
+    },
+    updateTime (e) {
+      this.currentTime = e.target.currentTime;
+    },
+    format (interval) {
+      interval = Math.floor(interval);
+      let minute = Math.floor(interval / 60);
+      let second = interval % 60;
+      second = second.toString().padStart(2, '0');
+      return `${minute}:${second}`;
+    },
+    onProgressBarChange (percent) {
+      this.$refs.audio.currentTime = percent * this.currentSong.duration;
+      if (!this.playing) {
+        this.togglePlaying();
+      }
+    },
     _getPosAndScale () {
       const targetWidth = 40;
       const paddingLeft = 40;
@@ -157,7 +230,8 @@ export default {
     },
     ...mapMutations({
       setFullScreen: 'SET_FULL_SCREEN',
-      setPlayingState: 'SET_PLAYING_STATE'
+      setPlayingState: 'SET_PLAYING_STATE',
+      setCurrentIndex: 'SET_CURRENT_INDEX'
     })
   },
   watch: {
@@ -176,183 +250,4 @@ export default {
 }
 </script>
 
-<style lang="stylus" rel="stylesheet/stylus">
-@import "~common/stylus/variable"
-@import "~common/stylus/mixin"
-
-.player
-  .normal-player
-    position fixed
-    left 0
-    right 0
-    top 0
-    bottom 0
-    z-index 150
-    background $color-background
-    .background
-      position absolute
-      left 0
-      top 0
-      width 100%
-      height 100%
-      z-index -1
-      opacity 0.6
-      filter blur(20px)
-    .top
-      position relative
-      margin-bottom 25px
-      .back
-        position absolute
-        top 0
-        left 6px
-        z-index 50
-        .icon-back
-          display block
-          padding 9px
-          font-size $font-size-large-x
-          color $color-theme
-          transform rotate(-90deg)
-      .title
-        width 70%
-        margin 0 auto
-        line-height 40px
-        text-align center
-        no-wrap()
-        font-size $font-size-large
-        color $color-text
-      .subtitle
-        line-height 20px
-        text-align center
-        font-size $font-size-medium
-        color $color-text
-    .middle
-      position fixed
-      width 100%
-      top 80px
-      bottom 170px
-      white-space nowrap
-      font-size 0
-      .middle-l
-        display inline-block
-        vertical-align top
-        position relative
-        width 100%
-        height 0
-        padding-top 80%
-        .cd-wrapper
-          position absolute
-          left 10%
-          top 0
-          width 80%
-          height 100%
-          .cd
-            width 100%
-            height 100%
-            box-sizing border-box
-            border 10px solid rgba(255, 255, 255, 0.1)
-            border-radius 50%
-            &.play
-              animation rotate 20s linear infinite
-            &.pause
-              animation-play-state paused
-            .image
-              position absolute
-              left 0
-              top 0
-              width 100%
-              height 100%
-              border-radius 50%
-    .bottom
-      position absolute
-      bottom 50px
-      width 100%
-      .operators
-        display flex
-        align-items center
-        .icon
-          flex 1
-          color $color-theme
-          &.disable
-            color $color-theme-d
-          i
-            font-size 30px
-        .i-left
-          text-align right
-        .i-center
-          padding 0 20px
-          text-align center
-          i
-            font-size 40px
-        .i-right
-          text-align left
-        .icon-favorite
-          color $color-sub-theme
-    &.normal-enter-active, &.normal-leave-active
-      transition all 0.4s
-      .top, .bottom
-        transition all 0.4s cubic-bezier(0.86, 0.18, 0.82, 1.32)
-    &.normal-enter, &.normal-leave-to
-      opacity 0
-      .top
-        transform translate3d(0, -100px, 0)
-      .bottom
-        transform translate3d(0, 100px, 0)
-  .mini-player
-    display flex
-    align-items center
-    position fixed
-    left 0
-    bottom 0
-    z-index 180
-    width 100%
-    height 60px
-    background $color-highlight-background
-    &.mini-enter-active, &.mini-leave-active
-      transition all 0.5s
-    &.mini-enter, &.mini-leave-to
-      opacity 0
-    .icon
-      flex 0 0 40px
-      width 40px
-      padding 0 10px 0 20px
-      img
-        border-radius 50%
-        &.play
-          animation rotate 10s linear infinite
-        &.pause
-          animation-play-state paused
-    .text
-      display flex
-      flex-direction column
-      justify-content center
-      flex 1
-      line-height 20px
-      overflow hidden
-      .name
-        margin-bottom 2px
-        no-wrap()
-        font-size $font-size-medium
-        color $color-text
-      .desc
-        no-wrap()
-        font-size $font-size-small
-        color $color-text-d
-    .control
-      flex 0 0 30px
-      width 30px
-      padding 0 10px
-      .icon-play-mini, .icon-pause-mini, .icon-playlist
-        font-size: 30px
-        color: $color-theme-d
-      .icon-mini
-        font-size: 32px
-        position: absolute
-        left: 0
-        top: 0
-
-@keyframes rotate
-  0%
-    transform rotate(0)
-  100%
-    transform rotate(360deg)
-</style>
+<style src="./player.styl" lang="stylus" rel="stylesheet/stylus"></style>
